21e8def62d2e802222a55387206c2356479ed2c9
vatsal gupta <62776100+vatsuvaksi@users.noreply.github.com>
2023-12-18 09:49:36 -0800

    Made GetReuest with SOLID

--- src/main/java/com/vatsuvaksi/clients/httpclients/GetRequest.java
+++ src/main/java/com/vatsuvaksi/clients/httpclients/GetRequest.java
@@ -6,71 +6,101 @@ import com.vatsuvaksi.requests.protocols.http.nonsecure.requestbody.HttpRequestB
 import com.vatsuvaksi.utils.json.JsonFactory;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 
-// TODO :  Break down the function into smaller funcctions
-public class GetRequest<T extends CliRequest , K> implements HttpClient<T , K> {
+public class GetRequest<T extends CliRequest, K> implements HttpClient<T, K> {
     private static final String GET = "GET";
+
     @Override
     public K executeRequest(HttpRequest httpRequest, Class<K> responseType) {
         HttpURLConnection con = null;
         try {
             URL url = httpRequest.getUrl().toURL();
-            con = (HttpURLConnection) url.openConnection();
+            con = openConnection(url);
 
+            setRequestMethod(con);
 
-            con.setRequestMethod(GET);
+            setRequestHeaders(con, httpRequest);
 
-            if((httpRequest.getHeaders() != null)){
-                for(Map.Entry<String, String> entry : httpRequest.getHeaders().entrySet()){
-                    con.addRequestProperty(entry.getKey() , entry.getValue());
-                }
-            }
-            // Not required as URL will set the query params hence commenting this for now
-            //** if(httpRequest.getQueryParams() != null){} **//
+            writeRequestBody(con, httpRequest);
 
-            // TODO :  Remove this and don't allow GET to have a request body
-            // TODO : Need to fetch according to instance
-            HttpRequestBody httpRequestBody = httpRequest.getHttpRequestBody();
-
-            if(httpRequestBody != null && httpRequestBody.getContent() != null){
-                con.setDoOutput(true);
-                try (OutputStream os = con.getOutputStream()) {
-                    String requestBodyContent = JsonFactory
-                            .convertObjectToJson(httpRequestBody.getContent());
-                    os.write(requestBodyContent.getBytes("UTF-8"));
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            con.setConnectTimeout(10000);
-
+            setConnectTimeout(con);
 
             int responseCode = con.getResponseCode();
-            if(responseCode == 200){
-                StringBuilder responseStringBuilder = new StringBuilder();
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-                    String inputLine;
-                    while ((inputLine = in.readLine()) != null) {
-                        responseStringBuilder.append(inputLine);
-                    }
-                }
-                return JsonFactory.convertJsonToObject(responseStringBuilder.toString() , responseType);
-            }else{
+            if (responseCode == 200) {
+                return processResponse(con, responseType);
+            } else {
                 throw new RuntimeException("Response Status Code Not Equal to OK[200]");
             }
-        }  catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
-            if(con != null){
-                con.disconnect();
-            }
+            disconnectSafely(con);
+        }
+    }
 
+    private HttpURLConnection openConnection(URL url) throws IOException {
+        return (HttpURLConnection) url.openConnection();
+    }
+
+    private void setRequestMethod(HttpURLConnection con) throws IOException {
+        con.setRequestMethod(GET);
+    }
+
+    private void setRequestHeaders(HttpURLConnection con, HttpRequest httpRequest) {
+        if (httpRequest.getHeaders() != null) {
+            for (Map.Entry<String, String> entry : httpRequest.getHeaders().entrySet()) {
+                con.addRequestProperty(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    private void writeRequestBody(HttpURLConnection con, HttpRequest httpRequest) {
+        HttpRequestBody httpRequestBody = httpRequest.getHttpRequestBody();
+        if (httpRequestBody != null && httpRequestBody.getContent() != null) {
+            try {
+                setDoOutput(con);
+                writeRequestBodyContent(con, httpRequestBody);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void setDoOutput(HttpURLConnection con) {
+        con.setDoOutput(true);
+    }
+
+    private void writeRequestBodyContent(HttpURLConnection con, HttpRequestBody httpRequestBody) throws Exception {
+        try (OutputStream os = con.getOutputStream()) {
+            String requestBodyContent = JsonFactory.convertObjectToJson(httpRequestBody.getContent());
+            os.write(requestBodyContent.getBytes("UTF-8"));
+        }
+    }
+
+    private void setConnectTimeout(HttpURLConnection con) {
+        con.setConnectTimeout(10000);
+    }
+
+    private K processResponse(HttpURLConnection con, Class<K> responseType) throws Exception {
+        StringBuilder responseStringBuilder = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                responseStringBuilder.append(inputLine);
+            }
+        }
+        return JsonFactory.convertJsonToObject(responseStringBuilder.toString(), responseType);
+    }
+
+    private void disconnectSafely(HttpURLConnection con) {
+        if (con != null) {
+            con.disconnect();
         }
     }
 }
-
